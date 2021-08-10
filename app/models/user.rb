ce27d06455f055c9658aae4ce4d8976de6085d1e
davidclaveau <daveclaveau@gmail.com>
2021-08-10 14:42:19 -0700

    Fix users and teams, set properties and relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   validates :password_digest, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :phone_number, presence: true
 end
--- db/migrate/20210810213434_create_users.rb
+++ db/migrate/20210810213434_create_users.rb
@@ -5,8 +5,8 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :password_digest, null: false
       t.string :first_name, null: false
       t.string :last_name, null: false
-      t.integer :phone_number, null: false
-      t.integer :team_id, foreign_key: true
+      t.string :phone_number, null: false
+      t.integer :team_id
       t.string :winter_team
       t.string :public_sector
 
